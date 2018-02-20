4d61e38a07c92aa92d5b8eeb8282340e0a9849b0
java-2020 <java2020uk@gmail.com>
2018-02-20 07:57:05 +0000

    first commit

---
+++ src/main/java/com/example/feb/demoFeb/DemoFebApplication.java
@@ -0,0 +1,14 @@
+package com.example.feb.demoFeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFebApplication.class, args);
+		
+		System.out.println("abc");
+	}
+}
